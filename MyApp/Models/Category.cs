105341859c9278cef42cc260f5ad3ab403fb664b
ranjit <ranjit@gmail.com>
2022-08-13 17:36:42 +0545

    Add Existing Files

--- MyApp/Models/Category.cs
+++ MyApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyApp.Models
 {
@@ -9,7 +10,8 @@ namespace MyApp.Models
 
         [Required]
         public string? Name { get; set; }
-
+       
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; } 
 
         public DateTime CreatedDate { get; set; } = DateTime.Now;
